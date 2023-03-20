9713bf15d6ed046a937994dedb6cf9756fad3cd4
badlyguy <98670441+badlyguy@users.noreply.github.com>
2023-03-20 17:24:44 -0600

    Add files via upload

--- CS476 Project WEY entertainment/manage-friend.php
+++ CS476 Project WEY entertainment/manage-friend.php
@@ -1,3 +1,16 @@
+<?php
+
+session_start();
+if (isset($_SESSION["user_id"])) {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = "SELECT * FROM friend
+            WHERE user_1 = {$_SESSION["user_id"]}";
+            
+    $result = $mysqli->query($sql);
+}
+?>
 <!DOCTYPE html> 
 <html> 
     <head>
@@ -37,25 +50,33 @@
             <table>
                 <tr class="evenRow">
                     <th>Name</th>
-                    <th>Introduction</th>
-                    <th>Start to Play</th>
+                    <th>Friend E-mail</th>
+                    <th>Talk with your Friend</th>
                 </tr>
-                <tr>
-                    <td>game a</td>
-                    <td>xxxxxxxxxxxxxxxxxxsxxsxsxsxs</td>
-                    <td><a href="gamepage.html">Play</a></td>
-                </tr>
-                </tr>
-                <tr>
-                    <td>game b</td>
-                    <td>xxsssssssssssssssssssxx</td>
-                    <td><a href="gamepage.html">Chat room</a></td>
-                </tr>
+                
+                <?php
+                    while ($row = $result->fetch_assoc()) {
+                        // Get friend information from user_2 table
+                        $friendId = $row["user_2"];
+                        $friendSql = "SELECT * FROM user WHERE id = '$friendId'";
+                        $friendResult = $mysqli->query($friendSql);
+                        $friendRow = $friendResult->fetch_assoc();
+                        
+                        // Display friend information in table row
+                        echo '<tr>';
+                        echo '<td>' . $friendRow['name'] . '</td>';
+                        echo '<td>' . $friendRow['email'] . '</td>';
+                        echo '<td><a href="chatroom.html">Chat room</a></td>';
+                        echo '</tr>';
+                    }
+                ?>
             </table>
         </div>
 
         <div style="text-align:center;">
             <p>
+                <a href="addfriend.php" class="niceButton">Go to Add New Friend</a>>
+                &emsp;&emsp;&emsp;&emsp;
                 <a href="index.php" class="niceButton">Back to my Home page</a>>
             </p>
         </div>
