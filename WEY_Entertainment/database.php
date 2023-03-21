cf77c866f7946c0120490790885a9520e7ffc580
DarthWalker2300 <96734721+DarthWalker2300@users.noreply.github.com>
2023-03-20 23:40:28 -0600

    Add files via upload

---
+++ WEY_Entertainment/database.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "sql9.freesqldatabase.com";
+$dbname = "sql9607367";
+$username = "sql9607367";
+$password = "mQQXyFvQAn";
+
+$mysqli = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
+                     
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
+}
+
+return $mysqli;
+?>
---
+++ WEY_Entertainment/login.php
@@ -0,0 +1,34 @@
+<?php
+
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = sprintf("SELECT * FROM user
+                WHERE email = '%s'",
+                $mysqli->real_escape_string($_POST["email"]));
+
+$result = $mysqli->query($sql);
+
+$user = $result->fetch_assoc();
+
+if ($user) {
+    
+    if (password_verify($_POST["password"], $user["password_hash"])) {
+        
+        session_start();
+        
+        session_regenerate_id();
+        
+        $_SESSION["user_id"] = $user["id"];
+        
+        header("Location: index.php");
+        exit;
+    }
+}
+else {
+    
+    
+    header("Location: login.html");
+}
+
+
+?>
